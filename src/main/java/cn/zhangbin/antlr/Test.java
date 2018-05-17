28d010c20e45dde6f5642bfaf61bef226968e59d
zhangbin <zhangbin3@yixa.com>
2018-05-17 11:23:10 +0800

    commit

---
+++ src/main/java/cn/zhangbin/antlr/Test.java
@@ -0,0 +1,10 @@
+package cn.zhangbin.antlr;
+
+import org.apache.hadoop.hive.ql.parse.ParseDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        ParseDriver pd = new ParseDriver();
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/cn/zhangbin/bean/Block.java
@@ -0,0 +1,21 @@
+package cn.zhangbin.bean;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Block {
+    private String condition;
+    private Set<String> colSet = new LinkedHashSet<String>();
+    public String getCondition() {
+        return condition;
+    }
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    public Set<String> getColSet() {
+        return colSet;
+    }
+    public void setColSet(Set<String> colSet) {
+        this.colSet = colSet;
+    }
+}
